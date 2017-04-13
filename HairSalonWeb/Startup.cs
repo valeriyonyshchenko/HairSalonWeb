5e5f9b769a9da39cb9c40eda545e636ff8eb898d
Valeriy Onyshchenko <valeriyonyshchenko@gmail.com>
2017-04-13 20:19:19 +0300

    Created project

---
+++ HairSalonWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HairSalonWeb.Startup))]
+namespace HairSalonWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
